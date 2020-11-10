a9dd371e4a29c855437269d0c1693e8fa1490d1f
matjaz99 <matjaz99@gmail.com>
2020-11-10 02:08:12 +0100

    update

---
+++ src/main/java/si/iskratel/metricslib/Alarm.java
@@ -0,0 +1,17 @@
+package si.iskratel.metricslib;
+
+public class Alarm {
+
+    private long time;
+    private int code;
+    private int priority;
+    private String source;
+    private String description;
+    private int alarmId;
+    private String codeAsAsString;
+    private String priorityAsString;
+    private int dmId;
+    private boolean internal;
+    private int instanceIndex;
+
+}
