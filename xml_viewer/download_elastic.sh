24e78370c108b8f040c3bf8b9e37186bc840d650
matjaz99 <matjaz99@gmail.com>
2021-05-13 15:46:38 +0200

    update

---
+++ xml_viewer/download_elastic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.12.1-linux-x86_64.tar.gz
+tar -zxf elasticsearch-7.12.1-linux-x86_64.tar.gz
+rm -f elasticsearch-7.12.1-linux-x86_64.tar.gz
+
+
+wget https://artifacts.elastic.co/downloads/kibana/kibana-7.12.1-linux-x86_64.tar.gz
+tar -zxf kibana-7.12.1-linux-x86_64.tar.gz
+rm -f kibana-7.12.1-linux-x86_64.tar.gz
