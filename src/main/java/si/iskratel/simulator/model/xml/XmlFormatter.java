a367f1254f5c5fb8dfa7cea49c79ca071992bd03
matjaz99 <matjaz99@gmail.com>
2022-06-01 00:35:48 +0200

    update

--- src/main/java/si/iskratel/simulator/model/xml/XmlFormatter.java
+++ src/main/java/si/iskratel/simulator/model/xml/XmlFormatter.java
@@ -16,24 +16,42 @@ public class XmlFormatter {
     public static CdrAggs getCdrAggsRootElement(CdrData data, String filename, PMultiValueMetric mv_metric) {
 
         CdrAggs cdrAggs = new CdrAggs();
-        CdrAggs.Metadata metadata = XmlFormatter.getMetadata(data, filename);
-
-        CdrAggs.Statistics statistics = new CdrAggs.Statistics();
 
         CdrAggs.Statistics.Node node = new CdrAggs.Statistics.Node();
         CdrAggs.Statistics.CallStats nodeCallStats = new CdrAggs.Statistics.CallStats();
         nodeCallStats.setCounters(getNodeCounters(mv_metric));
         nodeCallStats.setCauses(getNodeCauses(mv_metric));
+        node.setCallStats(nodeCallStats);
 
         CdrAggs.Statistics.TrunkGroups trunkGroups = new CdrAggs.Statistics.TrunkGroups();
         List<CdrAggs.Statistics.TrunkGroups.TrunkGroup> trunkGroupList = new ArrayList<>();
 
         for (PMultivalueTimeSeries ts : mv_metric.getMultivalueMetrics()) {
-            if (ts.getValuesMap().get("trunkGroup.seizures") != null) {
+            if (ts.getValuesMap().get("trunkGroup.records") != null) {
+
+                CdrAggs.Statistics.TrunkGroups.TrunkGroup tg = new CdrAggs.Statistics.TrunkGroups.TrunkGroup();
+                tg.setId(ts.getLabelsMap().getOrDefault("trunkGroup.id", "-"));
+                tg.setName(ts.getLabelsMap().getOrDefault("trunkGroup.name", "-"));
+                tg.setDirection(ts.getLabelsMap().getOrDefault("trunkGroup.direction", "-"));
 
                 CdrAggs.Statistics.CallStats callStats = new CdrAggs.Statistics.CallStats();
+                callStats.setCounters(getTrunkGroupCounters(ts));
+                callStats.setCauses(getTrunkGroupCauses(ts));
+
+                tg.setCallStats(callStats);
+                trunkGroupList.add(tg);
             }
         }
+
+        trunkGroups.setTrunkGroup(trunkGroupList);
+
+        CdrAggs.Statistics statistics = new CdrAggs.Statistics();
+        statistics.setNode(node);
+        statistics.setTrunkGroups(trunkGroups);
+        cdrAggs.setStatistics(statistics);
+
+        CdrAggs.Metadata metadata = XmlFormatter.getMetadata(data, filename);
+        cdrAggs.setMetadata(metadata);
 
         return cdrAggs;
 
@@ -165,62 +183,92 @@ public class XmlFormatter {
         return null;
     }
 
-    private static CdrAggs.Statistics.CallStats.Counters getTrunkGroupCounters(PMultiValueMetric mv_metric) {
+    private static CdrAggs.Statistics.CallStats.Counters getTrunkGroupCounters(PMultivalueTimeSeries ts) {
 
-        for (PMultivalueTimeSeries ts : mv_metric.getMultivalueMetrics()) {
+        CdrAggs.Statistics.CallStats.Counters tgCounters = new CdrAggs.Statistics.CallStats.Counters();
+        List<CdrAggs.Statistics.CallStats.Counters.Counter> tgCounterList = new ArrayList<>();
 
-            if (ts.getValuesMap().containsKey("trunkGroup.records")) {
+        CdrAggs.Statistics.CallStats.Counters.Counter tgRecords = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgRecords.setName("trunkGroup.records");
+        tgRecords.setValue(ts.getValuesMap().getOrDefault("trunkGroup.records", 0.0).longValue());
+        tgCounterList.add(tgRecords);
 
-                CdrAggs.Statistics.CallStats.Counters tgCounters = new CdrAggs.Statistics.CallStats.Counters();
-                List<CdrAggs.Statistics.CallStats.Counters.Counter> tgCounterList = new ArrayList<>();
+        CdrAggs.Statistics.CallStats.Counters.Counter tgSeizures = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgSeizures.setName("trunkGroup.seizures");
+        tgSeizures.setValue(ts.getValuesMap().getOrDefault("trunkGroup.seizures", 0.0).longValue());
+        tgCounterList.add(tgSeizures);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgRecords = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgRecords.setName("trunkGroup.records");
-                tgRecords.setValue(ts.getValuesMap().getOrDefault("trunkGroup.records", 0.0).longValue());
-                tgCounterList.add(tgRecords);
+        CdrAggs.Statistics.CallStats.Counters.Counter tgSeizWAns = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgSeizWAns.setName("trunkGroup.seizuresWithAnswer");
+        tgSeizWAns.setValue(ts.getValuesMap().getOrDefault("trunkGroup.seizuresWithAnswer", 0.0).longValue());
+        tgCounterList.add(tgSeizWAns);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgSeizures = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgSeizures.setName("trunkGroup.seizures");
-                tgSeizures.setValue(ts.getValuesMap().getOrDefault("trunkGroup.seizures", 0.0).longValue());
-                tgCounterList.add(tgSeizures);
+        CdrAggs.Statistics.CallStats.Counters.Counter tgActive = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgActive.setName("trunkGroup.activeCalls");
+        tgActive.setValue(ts.getValuesMap().getOrDefault("trunkGroup.activeCalls", 0.0).longValue());
+        tgCounterList.add(tgActive);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgSeizWAns = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgSeizWAns.setName("trunkGroup.seizuresWithAnswer");
-                tgSeizWAns.setValue(ts.getValuesMap().getOrDefault("trunkGroup.seizuresWithAnswer", 0.0).longValue());
-                tgCounterList.add(tgSeizWAns);
+        CdrAggs.Statistics.CallStats.Counters.Counter tgDur = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgDur.setName("trunkGroup.duration");
+        tgDur.setUnit("ms");
+        tgDur.setValue(ts.getValuesMap().getOrDefault("trunkGroup.duration", 0.0).longValue());
+        tgCounterList.add(tgDur);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgActive = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgActive.setName("trunkGroup.activeCalls");
-                tgActive.setValue(ts.getValuesMap().getOrDefault("trunkGroup.activeCalls", 0.0).longValue());
-                tgCounterList.add(tgActive);
+        CdrAggs.Statistics.CallStats.Counters.Counter tgTrInt = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgTrInt.setName("trunkGroup.trafficIntensity");
+        tgTrInt.setUnit("E");
+        tgTrInt.setValue(ts.getValuesMap().getOrDefault("trunkGroup.trafficIntensity", 0.0).longValue());
+        tgCounterList.add(tgTrInt);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgDur = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgDur.setName("trunkGroup.duration");
-                tgDur.setUnit("ms");
-                tgDur.setValue(ts.getValuesMap().getOrDefault("trunkGroup.duration", 0.0).longValue());
-                tgCounterList.add(tgDur);
+        CdrAggs.Statistics.CallStats.Counters.Counter tgTrVol = new CdrAggs.Statistics.CallStats.Counters.Counter();
+        tgTrVol.setName("trunkGroup.trafficVolume");
+        tgTrVol.setUnit("Eh");
+        tgTrVol.setValue(ts.getValuesMap().getOrDefault("trunkGroup.trafficVolume", 0.0).longValue());
+        tgCounterList.add(tgTrVol);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgTrInt = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgTrInt.setName("trunkGroup.trafficIntensity");
-                tgTrInt.setUnit("E");
-                tgTrInt.setValue(ts.getValuesMap().getOrDefault("trunkGroup.trafficIntensity", 0.0).longValue());
-                tgCounterList.add(tgTrInt);
+        tgCounters.setCounter(tgCounterList);
 
-                CdrAggs.Statistics.CallStats.Counters.Counter tgTrVol = new CdrAggs.Statistics.CallStats.Counters.Counter();
-                tgTrVol.setName("trunkGroup.trafficVolume");
-                tgTrVol.setUnit("Eh");
-                tgTrVol.setValue(ts.getValuesMap().getOrDefault("trunkGroup.trafficVolume", 0.0).longValue());
-                tgCounterList.add(tgTrVol);
+        return tgCounters;
+    }
 
-                tgCounters.setCounter(tgCounterList);
+    private static CdrAggs.Statistics.CallStats.Causes getTrunkGroupCauses(PMultivalueTimeSeries ts) {
 
-                return tgCounters;
+        CdrAggs.Statistics.CallStats.Causes causes = new CdrAggs.Statistics.CallStats.Causes();
+        List<CdrAggs.Statistics.CallStats.Causes.Cause> causeList = new ArrayList<>();
 
-            }
+        CdrAggs.Statistics.CallStats.Causes.Cause causeAns = new CdrAggs.Statistics.CallStats.Causes.Cause();
+        causeAns.setId("16");
+        causeAns.setName("answered");
+        causeAns.setValue(ts.getValuesMap().getOrDefault("trunkGroup.answered", 0.0).longValue());
+        causeList.add(causeAns);
 
-        }
+        CdrAggs.Statistics.CallStats.Causes.Cause causeBusy = new CdrAggs.Statistics.CallStats.Causes.Cause();
+        causeBusy.setId("17");
+        causeBusy.setName("busy");
+        causeBusy.setValue(ts.getValuesMap().getOrDefault("trunkGroup.busy", 0.0).longValue());
+        causeList.add(causeBusy);
 
-        return null;
+        CdrAggs.Statistics.CallStats.Causes.Cause causeNoResponse = new CdrAggs.Statistics.CallStats.Causes.Cause();
+        causeNoResponse.setId("17");
+        causeNoResponse.setName("noResponse");
+        causeNoResponse.setValue(ts.getValuesMap().getOrDefault("trunkGroup.noResponse", 0.0).longValue());
+        causeList.add(causeNoResponse);
+
+        CdrAggs.Statistics.CallStats.Causes.Cause causeRejected = new CdrAggs.Statistics.CallStats.Causes.Cause();
+        causeRejected.setId("21");
+        causeRejected.setName("rejected");
+        causeRejected.setValue(ts.getValuesMap().getOrDefault("trunkGroup.rejected", 0.0).longValue());
+        causeList.add(causeRejected);
+
+        CdrAggs.Statistics.CallStats.Causes.Cause causeOther = new CdrAggs.Statistics.CallStats.Causes.Cause();
+        causeOther.setId("0");
+        causeOther.setName("other");
+        causeOther.setValue(ts.getValuesMap().getOrDefault("trunkGroup.other", 0.0).longValue());
+        causeList.add(causeOther);
+
+        causes.setCause(causeList);
+
+        return causes;
     }
 
 }
